ce2f0ee4b351fa3ff8d3fb091127d4b231f275a5
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-01-20 16:32:38 +0100

    Added exercise n2

--- script2.js
+++ script2.js
@@ -11,6 +11,7 @@ Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengo
 
 */
 
+// # Creo un array 
 const clubs = [
    {name: 'Milan', points: 0, fouls: 0},
    {name: 'Roma', points: 0, fouls: 0},
@@ -19,3 +20,21 @@ const clubs = [
    {name: 'Atalanta', points: 0, fouls: 0},
 ];
 
+
+// # Ciclo for per girare nell'array 
+for(let i = 0; i < clubs.length; i++){
+   let randomPoint = Math.floor(Math.random() * 100) + 1;
+   let randomFoul = Math.floor(Math.random() * 30) + 1;
+   clubs[i].points = randomPoint;
+   clubs[i].fouls = randomFoul;
+}
+
+console.table(clubs);
+
+for(let j = 0; j < clubs.length; j++){
+
+   const nome = clubs[j].name;
+   const falli = clubs[j].fouls;
+   
+   console.table(nome, falli);
+}
