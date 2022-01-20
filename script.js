ffba982ec49292da6548a01f7f25b7fa40c1d94a
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-01-20 15:19:45 +0100

    Added files + array

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('JS OK!');
+
+/*
+
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare ain console la bici con peso minore utilizzando destructuring e template literal
+
+*/
+
+// # Costruisco l'array 
+const bikes = [
+   {name: 'Orbea', weight: 3},
+   {name: 'Pinarello', weight: 7},
+   {name: 'Speedoo', weight: 5},
+   {name: 'Bianchi', weight: 6},
+   {name: 'Merida', weight: 10},
+];
+
