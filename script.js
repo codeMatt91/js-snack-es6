7dff0ad72de466133e3b365bef546fb30b3066a7
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-01-20 19:09:23 +0100

    Fixed ex n1

--- script.js
+++ script.js
@@ -11,22 +11,37 @@ Stampare ain console la bici con peso minore utilizzando destructuring e templat
 
 // # Costruisco l'array 
 const bikes = [
-   {name: 'Orbea', weight: 11},
-   {name: 'Pinarello', weight: 7},
-   {name: 'Speedoo', weight: 5},
-   {name: 'Bianchi', weight: 6},
-   {name: 'Merida', weight: 10},
+   {name: 'Orbea', peso: 11},
+   {name: 'Pinarello', peso: 7},
+   {name: 'Speedoo', peso: 5},
+   {name: 'Bianchi', peso: 6},
+   {name: 'Merida', peso: 10},
 ];
 
 // # Ciclo per estrapolare tutti i pesi in un array 
-let light = [];
-for(let i = 0; i < bikes.length; i++){
 
-   let {weight} = bikes[i];
-   light.push(weight);
+let min = 0;
+for (let i = 0; i < bikes.length; i++) {
+const currentWeight = bikes[i].peso;
+if (i == 0) {
+    min = currentWeight;
 }
-console.log(light);
-
-// # Determino il più piccolo 
-const min = Math.min(...light);
+if (currentWeight < min) {
+    min = currentWeight;
+}
+};
 console.log(min);
+
+
+// # Ciclo con funzione Math.min 
+// let light = [];
+// for(let i = 0; i < bikes.length; i++){
+
+//    let {weight} = bikes[i];
+//    light.push(weight);
+// }
+// console.log(light);
+
+// // # Determino il più piccolo 
+// const min = Math.min(...light);
+// console.log(min);
