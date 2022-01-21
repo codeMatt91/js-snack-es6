a902b3b608bbd53c450f45e03cf70827a7cbea8e
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-01-21 18:04:10 +0100

    Added exercise 1

--- snack3.js
+++ snack3.js
@@ -13,18 +13,33 @@ Usiamo i nuovi metodi degli array visti oggi in classe.
 // # Creo un array 
 const students = ['Federico', 'Matteo', 'Pietro', 'Federico', 'Nico', 'Anna', 'Maria', 'Giovanni', 'Marco'];
 
-const element = [];
+// const element = [];
 
-function getElementsByArray(arrays,index1,index2){
+// function getElementsByArray(arrays,index1,index2){
    
-   for(let i = 0; i < arrays.length; i++){
-      if(i >= index1 && i <= index2){
-         element.push(arrays[i]);
+//    for(let i = 0; i < arrays.length; i++){
+//       if(i >= index1 && i <= index2){
+//          element.push(arrays[i]);
+//       }
+//    }
+//    return element;
+// }
+
+// getElementsByArray(students, 3, 5);
+
+// console.log(element)
+
+// # Ciclo in un array con il Filter 
+function getElementsByArray(arrays,a,b){
+   const newArray = arrays.filter((item, index) => {
+      if(index >= a && index <= b){
+         return true;
       }
-   }
-   return element;
-}
+      return false;
+   })
+   return newArray;
+};
 
-getElementsByArray(students, 3, 5);
+const newArray = getElementsByArray(students,3,5);
 
-console.log(element)
+console.log(newArray);
